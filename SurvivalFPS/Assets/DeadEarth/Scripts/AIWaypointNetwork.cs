da8ad812af2fb6789c235cf8caee28eb19da56ff
David Pilger <dpilger26@gmail.com>
2019-04-11 00:09:13 -0600

    Lecture 5

---
+++ SurvivalFPS/Assets/DeadEarth/Scripts/AIWaypointNetwork.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIWaypointNetwork : MonoBehaviour 
+{
+    // configuration parameters
+    [SerializeField] List<Transform> waypoints = new List<Transform>();
+
+    public List<Transform> GetWaypoints()
+    {
+        return waypoints;
+    }
+}
